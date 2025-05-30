4dbb7c3d30c7ead2a0df541129de02847e271b85
Alfaro294 <jenniferalfaro0819@gmail.com>
2025-05-30 15:22:32 -0600

    Prueba de CRUD WEB con API

---
+++ JS/script.js
@@ -0,0 +1,42 @@
+//Direcciòn del Endpoint generado en Retool
+const API_URL = "https://retoolapi.dev/427Q72/integrantes";
+
+//Funcion que llama a la API y realiza una solicitud GET, obtine  un JSON
+async function ObtenerRegistros() {
+    //Hacemos GET al servidor (API) y obtenemos su respuesta (response)
+    const respuesta = await fetch (API_URL);
+
+    //Obtenemos los datos en formato JSON a partir de la respuesta
+    const data = await respuesta.json();//Esto ya es un JSON lo que necesitabamos para poder crear la tabla
+
+    //Llamamos a MostrarRegistros y le enviamos el JSON
+    MostrarRegistros(data);
+}
+
+
+//Función para generar las filas de la tabla
+//'Datos' representa al JSON
+function MostrarRegistros(datos){
+    //Se llama al elemento tbody dentro de la tabla con id 'tabla'
+    const tabla = document.querySelector("#tabla tbody");
+
+    //Para inyectar codigo html usamos innerHTML
+    tabla.innerHTML = ""; //Vaciamos el contenido de la tabla
+
+    datos.forEach(persona => {
+        tabla.innerHTML += `
+        <tr>
+            <td>${persona.id}</td>
+            <td>${persona.nombre}</td>
+            <td>${persona.apellido}</td>
+            <td>${persona.correo}</td>
+            <td>
+            <button>Editar</button>
+            <button>Eliminar</button>
+            </td>
+        </tr>
+        `;
+    });
+}
+
+ObtenerRegistros();
